00cc79131e6ba7d5e8e47f8e5fee60cbddc0edac
Isaac <muse_i@MuseI-Opt990.wdl.wdc.com>
2013-12-27 15:17:09 -0700

    - Refactor tabs_extra code - Consolidate code

--- tabs_extra.py
+++ tabs_extra.py
@@ -3,14 +3,14 @@ import sublime
 
 
 def is_persistent():
-    return sublime.load_settings("tabs_extra.sublime-settings").get("persistent_marks", False)
+    return sublime.load_settings("tabs_extra.sublime-settings").get("persistent_sticky", False)
 
 
-def respect_marks():
-    return sublime.load_settings("tabs_extra.sublime-settings").get("all_commands_respect_marks", False)
+def respect_sticky():
+    return sublime.load_settings("tabs_extra.sublime-settings").get("all_commands_respect_sticky", False)
 
 
-class ClearAllTabMarksCommand(sublime_plugin.WindowCommand):
+class TabsExtraClearAllStickyCommand(sublime_plugin.WindowCommand):
     def run(self, group=-1, force=False):
         if group == -1:
             return
@@ -18,175 +18,104 @@ class ClearAllTabMarksCommand(sublime_plugin.WindowCommand):
         views = self.window.views_in_group(group)
         if not persistent or force:
             for v in views:
-                v.settings().erase("tabs_extra_marked")
+                v.settings().erase("tabs_extra_sticky")
 
     def is_visible(self, group=-1, force=False):
         marked = False
         views = self.window.views_in_group(group)
         for v in views:
-            if v.settings().get("tabs_extra_marked", False):
+            if v.settings().get("tabs_extra_sticky", False):
                 marked = True
                 break
         return marked
 
 
-class CloseTabCommand(sublime_plugin.WindowCommand):
-    def run(self, group=-1, index=-1):
-        if group == -1 or index == -1:
-            return
-        persistent = is_persistent()
-        check_marks = respect_marks
-        views = self.window.views_in_group(group)
-        v = views.pop(index)
-        if not check_marks or (check_marks and not v.settings().get("tabs_extra_marked", False)):
-            if not persistent:
-                v.settings().erase("tabs_extra_marked")
-            self.window.focus_view(v)
-            self.window.run_command("close_file")
-        elif not persistent:
-            v.settings().erase("tabs_extra_marked")
-
-        # Clear all marks
-        if not persistent and len(views):
-            self.window.run_command("clear_all_tab_marks", {"group": group})
-
-
-class CloseTabsAllCommand(sublime_plugin.WindowCommand):
-    def run(self, group=-1, index=-1):
-        if group == -1 or index == -1:
-            return
-        persistent = is_persistent()
-        check_marks = respect_marks
-        views = self.window.views_in_group(group)
-        for v in views:
-            if not check_marks or (check_marks and not v.settings().get("tabs_extra_marked", False)):
-                if not persistent:
-                    v.settings().erase("tabs_extra_marked")
-                self.window.focus_view(v)
-                self.window.run_command("close_file")
-            elif not persistent:
-                v.settings().erase("tabs_extra_marked")
-
-
-class MarkTabCommand(sublime_plugin.WindowCommand):
+class TabsExtraToggleStickyCommand(sublime_plugin.WindowCommand):
     def run(self, group=-1, index=-1):
         if group == -1 or index == -1:
             return False
         view = self.window.views_in_group(group)[index]
-        if not view.settings().get("tabs_extra_marked", False):
-            view.settings().set("tabs_extra_marked", True)
+        if not view.settings().get("tabs_extra_sticky", False):
+            view.settings().set("tabs_extra_sticky", True)
         else:
-            view.settings().erase("tabs_extra_marked")
+            view.settings().erase("tabs_extra_sticky")
 
     def is_checked(self, group=-1, index=-1):
         if group == -1 or index == -1:
             return False
-        return self.window.views_in_group(group)[index].settings().get("tabs_extra_marked", False)
+        return self.window.views_in_group(group)[index].settings().get("tabs_extra_sticky", False)
 
 
-class CloseTabsMarkedCommand(sublime_plugin.WindowCommand):
-    def run(self, group=-1, index=-1):
+class TabsExtraCloseStickyCommand(sublime_plugin.WindowCommand):
+    def run(self, group=-1, index=-1, invert=False):
         if group == -1 or index == -1:
             return
         persistent = is_persistent()
         views = self.window.views_in_group(group)
         for v in views:
-            if v.settings().get("tabs_extra_marked", False):
+            sticky = v.settings().get("tabs_extra_sticky", False)
+            if (not invert and sticky) or (invert and not sticky):
                 if not persistent:
-                    v.settings().erase("tabs_extra_marked")
+                    v.settings().erase("tabs_extra_sticky")
                 self.window.focus_view(v)
                 self.window.run_command("close_file")
             elif not persistent:
-                v.settings().erase("tabs_extra_marked")
+                v.settings().erase("tabs_extra_sticky")
 
 
-class CloseTabsUnmarkedCommand(sublime_plugin.WindowCommand):
-    def run(self, group=-1, index=-1):
+class TabsExtraCommand(sublime_plugin.WindowCommand):
+    def init(self, close_type, group, index):
+        self.persistent = is_persistent()
+        self.check_sticky = respect_sticky()
+        views = self.window.views_in_group(group)
+        assert(close_type in ["single", "left", "right", "other", "all"])
+
+        if close_type == "single":
+            self.targets = [views.pop(index)]
+            self.cleanup = bool(len(views[:index] + views[index + 1:]))
+        elif close_type == "left":
+            self.targets = views[:index]
+            self.cleanup = bool(len(views[index:]))
+        elif close_type == "right":
+            self.targets = views[index + 1:]
+            self.cleanup = bool(len(views[:index + 1]))
+        elif close_type == "other":
+            self.targets = views[:index] + views[index + 1:]
+            self.cleanup = True
+        elif close_type == "all":
+            self.targets = views[:]
+            self.cleanup = False
+
+    def run(self, group=-1, index=-1, close_type="single"):
         if group == -1 or index == -1:
             return
-        persistent = is_persistent()
-        views = self.window.views_in_group(group)
-        for v in views:
-            if not v.settings().get("tabs_extra_marked", False):
-                if not persistent:
-                    v.settings().erase("tabs_extra_marked")
+        self.init(close_type, group, index)
+
+        for v in self.targets:
+            if not self.check_sticky or (self.check_sticky and not v.settings().get("tabs_extra_sticky", False)):
+                if not self.persistent:
+                    v.settings().erase("tabs_extra_sticky")
                 self.window.focus_view(v)
                 self.window.run_command("close_file")
-            elif not persistent:
-                v.settings().erase("tabs_extra_marked")
+            elif not self.persistent:
+                v.settings().erase("tabs_extra_sticky")
 
-
-class CloseTabsToLeftCommand(sublime_plugin.WindowCommand):
-    def run(self, group=-1, index=-1):
-        if group == -1 or index == -1:
-            return
-        persistent = is_persistent()
-        check_marks = respect_marks()
-        views = self.window.views_in_group(group)
-        for v in views[:index]:
-            if not check_marks or (check_marks and not v.settings().get("tabs_extra_marked", False)):
-                if not persistent:
-                    v.settings().erase("tabs_extra_marked")
-                self.window.focus_view(v)
-                self.window.run_command("close_file")
-            elif not persistent:
-                v.settings().erase("tabs_extra_marked")
-
-        if not persistent and len(views[index:]):
-            self.window.run_command("clear_all_tab_marks", {"group": group})
-
-
-class CloseTabsToRightCommand(sublime_plugin.WindowCommand):
-    def run(self, group=-1, index=-1):
-        if group == -1 or index == -1:
-            return
-        persistent = is_persistent()
-        check_marks = respect_marks()
-        views = self.window.views_in_group(group)
-        if len(views) > index:
-            for v in self.window.views_in_group(group)[index + 1:]:
-                if not check_marks or (check_marks and not v.settings().get("tabs_extra_marked", False)):
-                    if not persistent:
-                        v.settings().erase("tabs_extra_marked")
-                    self.window.focus_view(v)
-                    self.window.run_command("close_file")
-                elif not persistent:
-                    v.settings().erase("tabs_extra_marked")
-
-        if not persistent and len(views[:index + 1]):
-            self.window.run_command("clear_all_tab_marks", {"group": group})
-
-
-class CloseTabsOtherCommand(sublime_plugin.WindowCommand):
-    def run(self, group=-1, index=-1):
-        if group == -1 or index == -1:
-            return
-        persistent = is_persistent()
-        check_marks = respect_marks()
-        views = self.window.views_in_group(group)
-        views.pop(index)
-        for v in views:
-            if not check_marks or (check_marks and not v.settings().get("tabs_extra_marked", False)):
-                if not persistent:
-                    v.settings().erase("tabs_extra_marked")
-                self.window.focus_view(v)
-                self.window.run_command("close_file")
-            elif not persistent:
-                v.settings().erase("tabs_extra_marked")
-
-        if not persistent:
-            self.window.run_command("clear_all_tab_marks", {"group": group})
+        if not self.persistent and self.cleanup:
+            self.window.run_command("tabs_extra_clear_all_sticky", {"group": group})
 
 
 class TabsExtraListener(sublime_plugin.EventListener):
     def on_window_command(self, window, command_name, args):
         if command_name == "close_by_index":
-            command_name = "close_tab"
+            command_name = "tabs_extra"
+            args["close_type"] = "single"
             return (command_name, args)
         elif command_name == "close_others_by_index":
-            command_name = "close_tabs_other"
+            command_name = "tabs_extra"
+            args["close_type"] = "other"
             return (command_name, args)
         elif command_name == "close_to_right_by_index":
-            command_name = "close_tabs_to_right"
+            command_name = "tabs_extra"
+            args["close_type"] = "right"
             return (command_name, args)
         return None
