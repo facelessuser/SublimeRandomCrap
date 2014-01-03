4dc60376c91d6f8448bac7945529b9b8d28f711c
facelessuser <faceless.shop@gmail.com>
2014-01-02 21:55:15 -0700

    - Growl enabled setting in settings file - Add “IsReady” command

--- subnotify.py
+++ subnotify.py
@@ -6,14 +6,41 @@ import sublime_plugin
 import SublimeRandomCrap.icons as icons
 
 notify = None
+PLUGIN_SETTINGS = "sub_notify.sublime-settings"
+SUB_NOTIFY_READY = False
+
+
+def log(msg, status=False):
+    string = str(msg)
+    print("SubNotify: %s" % string)
+    if status:
+        sublime.status_message(string)
+
+
+def debug_log(s):
+    if sublime.load_settings(PLUGIN_SETTINGS).get("debug", False):
+        log(s)
+
 
 class SubNotifyInfoCommand(sublime_plugin.ApplicationCommand):
     def run(self, title="", msg=""):
         notify.info(title, msg)
 
 
+class SubNotifyIsReadyCommand(sublime_plugin.ApplicationCommand):
+    @classmethod
+    def is_ready(cls):
+        return SUB_NOTIFY_READY
+
+    def run(self):
+        ready = SubNotifyIsReadyCommand.is_ready()
+        if ready:
+            log("Ready for messages!")
+
+
 def plugin_loaded():
     global notify
+    global SUB_NOTIFY_READY
     sys.path.insert(0, join(sublime.packages_path(), "SublimeRandomCrap", "gntp"))
 
     from SublimeRandomCrap.lib import notify
@@ -22,4 +49,5 @@ def plugin_loaded():
     if not exists(user):
         makedirs(user)
     notify.setup_notifications("Sublime Text", icons.notify_png.GetData(), None, user)
-    notify.enable_growl(True)
+    notify.enable_growl(sublime.load_settings(PLUGIN_SETTINGS).get("enable_growl", False))
+    SUB_NOTIFY_READY = True
