cda570c3731657f179150f352d0acd3764cbf36d
Isaac <muse_i@MuseI-Opt990.wdl.wdc.com>
2014-01-02 18:59:04 -0700

    -Drop in notify.py from Rummage -Add the GNTP package to allow python access to growl -Write a simple wrapper for Sublime. -Add sublime bubble icons

---
+++ subnotify.py
@@ -0,0 +1,25 @@
+import sys
+from os.path import join, exists
+from os import makedirs
+import sublime
+import sublime_plugin
+import SublimeRandomCrap.icons as icons
+
+notify = None
+
+class SubNotifyInfoCommand(sublime_plugin.ApplicationCommand):
+    def run(self, title="", msg=""):
+        notify.info(title, msg, True)
+
+
+def plugin_loaded():
+    global notify
+    sys.path.insert(0, join(sublime.packages_path(), "SublimeRandomCrap", "gntp"))
+
+    from SublimeRandomCrap.lib import notify
+
+    user = join(sublime.packages_path(), "User", "SubNotify")
+    if not exists(user):
+        makedirs(user)
+    notify.setup_notifications("Sublime Text", icons.notify_png.GetData(), None, user)
+    notify.enable_growl(True)
