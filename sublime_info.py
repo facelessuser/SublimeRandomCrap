f452a66948768a53bd30a8612c828359b71ac9ef
facelessuser <faceless.shop@gmail.com>
2014-06-20 20:05:56 -0600

    Add external ip

--- sublime_info.py
+++ sublime_info.py
@@ -1,6 +1,9 @@
 import socket
 import sublime
 import sublime_plugin
+import urllib.request as urllibReq
+import traceback
+
 
 INFO = '''
 Platform:        %(platform)s
@@ -8,7 +11,18 @@ Hostname:        %(hostname)s
 Sublime Version: %(version)s
 Architecture:    %(arch)s
 IP:              %(ip)s
+External IP:     %(e_ip)s
 '''
+
+
+def external_ip():
+    e_ip = "???"
+    try:
+        with urllibReq.urlopen("http://myip.dnsdynamic.org/") as url:
+            e_ip = url.read().decode("utf-8")
+    except:
+        print(traceback.format_exc())
+    return e_ip
 
 
 class SublimeInfoCommand(sublime_plugin.ApplicationCommand):
@@ -18,7 +32,8 @@ class SublimeInfoCommand(sublime_plugin.ApplicationCommand):
             "hostname": socket.gethostname().lower(),
             "version": sublime.version(),
             "arch": sublime.arch(),
-            "ip": socket.gethostbyname(socket.gethostname())
+            "ip": socket.gethostbyname(socket.gethostname()),
+            "e_ip": external_ip()
         }
 
         msg = INFO % info
