a9c74102be0e66edfcd59e896d716766c85009b4
Isaac <isaacmuse@gmail.com>
2014-09-26 16:46:32 -0600

    Update

--- sublime_pymdown.py
+++ sublime_pymdown.py
@@ -24,7 +24,7 @@ else:
 def notify(msg):
     settings = sublime.load_settings("reg_replace.sublime-settings")
     if settings.get("use_sub_notify", False) and Notify.is_ready():
-        sublime.run_command("sub_notify", {"title": "Mdown", "msg": msg})
+        sublime.run_command("sub_notify", {"title": "PyMdown", "msg": msg})
     else:
         sublime.status_message(msg)
 
@@ -32,9 +32,9 @@ def notify(msg):
 def error(msg):
     settings = sublime.load_settings("reg_replace.sublime-settings")
     if settings.get("use_sub_notify", False) and Notify.is_ready():
-        sublime.run_command("sub_notify", {"title": "Mdown", "msg": msg, "level": "error"})
+        sublime.run_command("sub_notify", {"title": "PyMdown", "msg": msg, "level": "error"})
     else:
-        sublime.error_message("Mdown:\n%s" % msg)
+        sublime.error_message("PyMdown:\n%s" % msg)
 
 
 def parse_file_name(file_name):
@@ -47,12 +47,12 @@ def parse_file_name(file_name):
     return title, basepath
 
 
-class MdownInsertText(sublime_plugin.TextCommand):
+class PyMdownInsertText(sublime_plugin.TextCommand):
     wbfr = None
 
     def run(self, edit, save=False):
-        self.view.insert(edit, 0, MdownInsertText.wbfr)
-        MdownInsertText.clear_wbfr()
+        self.view.insert(edit, 0, PyMdownInsertText.wbfr)
+        PyMdownInsertText.clear_wbfr()
         if save:
             self.view.run_command('save')
 
@@ -65,13 +65,13 @@ class MdownInsertText(sublime_plugin.TextCommand):
         cls.wbfr = None
 
 
-class MdownCommand(sublime_plugin.TextCommand):
+class PyMdownCommand(sublime_plugin.TextCommand):
     message = "<placeholder>"
 
     def setup(self, alternate_settings=None):
         self.title, self.basepath = parse_file_name(self.view.file_name())
-        self.binary = sublime.load_settings("mdown.sublime-settings").get("binary", {}).get(_PLATFORM, "")
-        self.reject = sublime.load_settings("mdown.sublime-settings").get("critic_reject", False)
+        self.binary = sublime.load_settings("pymdown.sublime-settings").get("binary", {}).get(_PLATFORM, "")
+        self.reject = sublime.load_settings("pymdown.sublime-settings").get("critic_reject", False)
         self.settings = alternate_settings
         self.cmd = [self.binary, "--title=%s" % self.title]
         if self.basepath is not None:
@@ -115,8 +115,8 @@ class MdownCommand(sublime_plugin.TextCommand):
         error(self.message)
 
 
-class MdownPreviewCommand(MdownCommand):
-    message = "mdown failed to generate html!"
+class PyMdownPreviewCommand(PyMdownCommand):
+    message = "pymdown failed to generate html!"
 
     def run(self, edit, target="browser", plain=False, alternate_settings=None):
         self.setup(alternate_settings)
@@ -129,6 +129,7 @@ class MdownPreviewCommand(MdownCommand):
         if self.target == "browser":
             # Nothing to do
             notify("Conversion complete!\nOpening in browser...")
+            # print(rbfr)
         elif self.target == "clipboard":
             sublime.set_clipboard(rbfr)
             notify("Conversion complete!\nResult copied to clipbaord.")
@@ -136,8 +137,8 @@ class MdownPreviewCommand(MdownCommand):
             window = self.view.window()
             if window is not None:
                 view = window.new_file()
-                MdownInsertText.set_wbfr(rbfr)
-                view.run_command("mdown_insert_text")
+                PyMdownInsertText.set_wbfr(rbfr)
+                view.run_command("pymdown_insert_text")
                 notify("Conversion complete!\nResult exported to Sublime.")
             else:
                 error("Could not export!\nView has no window")
@@ -149,8 +150,8 @@ class MdownPreviewCommand(MdownCommand):
                 if window is not None:
                     view = window.new_file()
                     if view is not None:
-                        MdownInsertText.set_wbfr(rbfr)
-                        view.run_command("mdown_insert_text", {"save": True})
+                        PyMdownInsertText.set_wbfr(rbfr)
+                        view.run_command("pymdown_insert_text", {"save": True})
                         notify("Conversion complete!\nReady to save...")
                     else:
                         error("Failed to create new view!")
@@ -191,8 +192,8 @@ class MdownPreviewCommand(MdownCommand):
             self.output(rbfr)
 
 
-class MdownCriticPreviewCommand(MdownCommand):
-    message = "mdown failed to generate your preview!"
+class PyMdownCriticPreviewCommand(PyMdownCommand):
+    message = "pymdown failed to generate your preview!"
 
     def run(self, edit, alternate_settings=None):
         self.setup(alternate_settings)
@@ -210,8 +211,8 @@ class MdownCriticPreviewCommand(MdownCommand):
             notify("Conversion complete!\nOpening in browser...")
 
 
-class MdownCriticStripCommand(MdownCommand):
-    message = "mdown failed to strip your critic comments!"
+class PyMdownCriticStripCommand(PyMdownCommand):
+    message = "pymdown failed to strip your critic comments!"
 
     def run(self, edit, reject=False, alternate_settings=None):
         self.setup(alternate_settings)
