a212ef64600fc0157028d4afde677a61917673fe
Isaac <isaacmuse@gmail.com>
2015-02-04 17:35:54 -0700

    Imporove link url

--- url_open.py
+++ url_open.py
@@ -2,16 +2,16 @@ import sublime_plugin
 import webbrowser
 import re
 
-RE_LINK = r'''(?x)(?i)
-(
-    (
-        http(s?)://(([a-zA-Z0-9\-._]+(\.[a-zA-Z0-9\-._]+)+)|localhost)|  # http://
-        (?P<www>w{3})(\.[a-zA-Z0-9\-._]+(\.[a-zA-Z0-9\-._]+)+)           # www.
+rex = re.compile(
+    r'''(?x)(?i)
+    \b(?:
+        https?://(?:(?:[a-z\d\-_]+(?:\.[a-z\d\-._]+)+)|localhost)|  # http://
+        w{3}\.[a-z\d\-_]+(?:\.[a-z\d\-._]+)+                        # www.
     )
-    (/?)([a-zA-Z0-9\-.?,'/+&%$#_]*)([\d\w./%+-=&?:"',|~;]*)
-    [A-Za-z\d\-_~:/?#@!$*+=]
+    /?[a-z\d\-._?,!'(){}\[\]/+&@%$#=:"|~;]*                         # url path and querry stuff
+    [a-z\d\-_~:/#@$*+=]                                             # allowed end chars
+    '''
 )
-'''
 
 
 class OpenContextUrlCommand(sublime_plugin.TextCommand):
@@ -31,14 +31,15 @@ class OpenContextUrlCommand(sublime_plugin.TextCommand):
 
         text = self.view.substr(line)
 
-        it = re.finditer(RE_LINK, text)
+        it = rex.finditer(text)
 
         for match in it:
             if match.start() <= (pt - line.a) and match.end() >= (pt - line.a):
                 url = text[match.start():match.end()]
-                if match.group('www') is not None:
-                    url = 'http://' + url
-                return url
+                if url[0:3] == "www":
+                    return "http://" + url
+                else:
+                    return url
 
         return None
 
