d210da013d675a78be50e65770d3185496f55ca8
facelessuser <faceless.shop@gmail.com>
2015-11-15 13:03:41 -0700

    Update docs

--- docs/index.md
+++ docs/index.md
@@ -8,15 +8,15 @@ A random collection of Sublime Text plugins.
 Sublime random crap is a collection of Sublime Text plugins that I either do no want to publicly support or that are in an experimental state.
 
 # Plugins
-- [AsciiTable (STABLE)](asciitable/)
-- [GrepHere (STABLE)](grephere/)
-- [HighlightWord (STABLE)](highlightword/)
-- [InsertDate (STABLE)](insertdate/)
-- [Postmaster (EXPERIMENTAL)](postmaster/)
-- [ShortcutPlus (STABLE)](shortcutplus/)
-- [SublimeApiDoc (STABLE)](sublimeapidoc/)
-- [SublimeInfo (STABLE)](sublimeinfo/)
-- [ToggleWhiteSpace (STABLE)](togglewhitespace/)
+- [AsciiTable (STABLE)](plugins/asciitable.md)
+- [GrepHere (STABLE)](plugins/grephere.md)
+- [HighlightWord (STABLE)](plugins/highlightword.md)
+- [InsertDate (STABLE)](plugins/insertdate.md)
+- [Postmaster (EXPERIMENTAL)](plugins/postmaster.md)
+- [ShortcutPlus (STABLE)](plugins/shortcutplus.md)
+- [SublimeApiDoc (STABLE)](plugins/sublimeapidoc.md)
+- [SublimeInfo (STABLE)](plugins/sublimeinfo.md)
+- [ToggleWhiteSpace (STABLE)](plugins/togglewhitespace.md)
 
 # Installation
 Use Package Control
--- popuptest.py
+++ popuptest.py
@@ -1,55 +1,22 @@
 """Popup test module."""
+import sublime
 import sublime_plugin
 import mdpopups
 
-test_content2 ='''
-
-- **Test** inline highlight ```:::python yield 0, '<code class="%s">' % self.cssclass```.
-- *Test* fenced highlight
-
-    ```html
-    <span style="color: #FFCC66;">SublimeInlineHtmlFormatter</span>
-    ```
-
-    ```python
-    class SublimeInlineHtmlFormatter(HtmlFormatter):
-    """Format the code blocks."""
-
-    def wrap(self, source, outfile):
-        """Overload wrap."""
-
-        return self._wrap_code(source)
-    ```
-
-    ```js hl_lines="1 3"
-    function isUndef(obj) {
-      return obj === void 0;
-    }
-
-    function isNull(obj) {
-      return obj === null;
-    }
-    ```
-
-'''
-
-insert_text = '''function isUndef(obj) {
-  return obj === void 0;
-}
-
-function isNull(obj) {
-  return obj === null;
-}
-'''
-
 test_content = '''
-# Testing Headers
+# H1
+## H2
+### H3
+#### H4
+##### H5
+###### H6
+Testing **bold** text and testing *italic* text.
+Testing autolink: www.google.com.
+Testing inline `code`.
 
-Testing **bold** text and testing *italic* text. Testing a link: www.google.com.  Testing inline `code`.
+    Testing indented code blocks
 
 ```
-Testing a fenced block.
-
 Testing a fenced block.
 ```
 
@@ -61,14 +28,11 @@ Testing
 
     Lists
 
-![Testing images](res://Packages/ColorHelper/tt_theme/images/back_dark.png)
-[Testing sublime href commands](sublime:whatever;3)
-
 - Testing lists
     - Testing lists
     - Testing lists
 
-- Testing lists
+- Testing ordered lists (ST will not show numbers :()
     1. Testing lists
     2. Testing lists
 
@@ -83,38 +47,6 @@ Testing
         """Overload wrap."""
 
         return self._wrap_code(source)
-
-    def _wrap_code(self, source):
-        """
-        Wrap the pygmented code.
-
-        Sublime popups don't really support 'code', but since it doesn't
-        hurt anything, we leave it in for the possiblity of future support.
-        We get around the lack of proper 'code' support by converting any
-        spaces after the intial space to nbsp.  We go ahead and convert tabs
-        to 4 spaces as well.
-        """
-
-        yield 0, '<code class="%s">' % self.cssclass
-        for i, t in source:
-            text = ''
-            matched = False
-            for m in html_re.finditer(t):
-                matched = True
-                if m.group(1):
-                    text += m.group(1)
-                elif m.group(3):
-                    text += m.group(3)
-                else:
-                    text += multi_space.sub(
-                        replace_nbsp, m.group(2).replace('\t', ' ' * 4)
-                    ).replace('&#39;', '\'').replace('&quot;', '"')
-            if not matched:
-                text = multi_space.sub(
-                    replace_nbsp, t.replace('\t', ' ' * 4)
-                ).replace('&#39;', '\'').replace('&quot;', '"')
-            yield i, text
-        yield 0, '</code>'
     ```
 
 ---
@@ -138,17 +70,21 @@ Testing
     Testing, admonition blocks.
 '''
 
-more = '''
-'''
-
 
 class MdPopupTestCommand(sublime_plugin.TextCommand):
     """Markdown Popup Test Command."""
 
-    def run(self, edit):
+    def run(self, edit, st_highlight=False):
         """Run command."""
 
+        settings = sublime.load_settings('Preferences.sublime-settings')
+        original = settings.get('mdpopups_use_sublime_highlighter', False)
+        if not st_highlight:
+            settings.set('mdpopups_use_sublime_highlighter', False)
+        else:
+            settings.set('mdpopups_use_sublime_highlighter', True)
         mdpopups.show_popup(
-            self.view, test_content2, css=more,
+            self.view, test_content,
             max_width=700, max_height=500,
         )
+        settings.set('mdpopups_use_sublime_highlighter', original)
