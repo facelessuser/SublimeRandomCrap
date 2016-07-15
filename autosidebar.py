0a561adf61355e47963ba6a59f561b26ccb1d9e8
Isaac <isaacmuse@gmail.com>
2016-07-15 16:31:50 -0600

    Cleanup and add tabbar experiment

--- autosidebar.py
+++ autosidebar.py
@@ -2,6 +2,8 @@
 import sublime
 import sublime_plugin
 
+tab_enabled = False
+
 
 class AutoSideBarListener(sublime_plugin.EventListener):
     """Listener that auto hides/shows the sidebar."""
@@ -15,10 +17,27 @@ class AutoSideBarListener(sublime_plugin.EventListener):
         Events like "reveal file" in sidebar will usually show the sidebar on their own.
         """
         win = view.window()
-
-        sidebar_visible = win.is_sidebar_visible()
-        if sidebar_visible is not None:
+        if win is not None:
+            sidebar_visible = win.is_sidebar_visible()
             if sidebar_visible is False and hover_zone == sublime.HOVER_GUTTER:
                 win.run_command('toggle_side_bar')
             elif sidebar_visible is True and hover_zone != sublime.HOVER_GUTTER:
                 win.run_command('toggle_side_bar')
+
+
+class AutoTabBarListener(sublime_plugin.EventListener):
+    """Listener that auto hides/shows the sidebar."""
+
+    def on_hover(self, view, point, hover_zone):
+        """Hide/Show tabs on hover event."""
+        if tab_enabled:
+            win = view.window()
+            y = view.viewport_position()[1]
+            point_y = view.text_to_layout(point)[1]
+            top_view = hover_zone != sublime.HOVER_GUTTER and (point_y - y) <= 1.0
+            if win is not None:
+                tabs_visible = win.get_tabs_visible()
+                if not tabs_visible and top_view:
+                    win.run_command('toggle_tabs')
+                elif tabs_visible and not top_view:
+                    win.run_command('toggle_tabs')
